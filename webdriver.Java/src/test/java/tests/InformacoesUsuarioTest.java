833b4abf1a7f797d8819b4f830e4c5a802879cd0
QALair <93294785+QALair@users.noreply.github.com>
2021-11-02 17:20:59 -0300

    Rev 1.3
    Finished! all tests running smooth.

--- webdriver.Java/src/test/java/tests/InformacoesUsuarioTest.java
+++ webdriver.Java/src/test/java/tests/InformacoesUsuarioTest.java
@@ -9,7 +9,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -18,34 +20,31 @@ public class InformacoesUsuarioTest {
 
     @Before
     public void setup() {
-        // setting the chromedriver location
+        /* setting the chromedriver location */
         System.setProperty("webdriver.chrome.driver", "D:\\Driver\\chromedriver.exe");
-        // creating a new variable and actually opening chrome already
+        /* creating a new variable and actually opening chrome already */
         nChrome = new ChromeDriver();
         nChrome.manage().window().maximize();
         nChrome.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-        //opening my first page, using teacher's webpage
+        /* opening my first page, using teacher's webpage */
         nChrome.get("http://www.juliodelima.com.br/taskit");
     }
 
     @Test
     public void testLogin(){
-        // click at the link that contains the text "sign in"
-        // If it was smth that I was going to use a lot :
-        //WebElement linkSignIn = nChrome.findElement(By.linkText("Sign in"));
-        //linkSignIn.click();
+        /* click at the link that contains the text "sign in" */
         nChrome.findElement(By.linkText("Sign in")).click(); // he is teaching to use the text element to find, but I already knew about xpath
 
-        // type the login
+        /* type the login */
         WebElement formularioSignInBox = nChrome.findElement(By.id("signinbox"));
         formularioSignInBox.findElement(By.name("login")).sendKeys("joaotn");
-        // type the password
+        /* type the password */
         formularioSignInBox.findElement(By.name("password")).sendKeys("A123b$"); // some rando password, didnt know who access this db
-        // sign in click
+        /* sign in click */
         nChrome.findElement(By.linkText("SIGN IN")).click();
 
-        // identify that it is me who is logged by element
+        /* identify that it is me who is logged by element */
         WebElement me = nChrome.findElement(By.className("me"));
         String textoNoElementoMe = me.getText();
 
@@ -55,49 +54,75 @@ public class InformacoesUsuarioTest {
     // new test case, testing myself for doing smth alone, and it worked, java seems to be easy and cool
     @Test
     public void testMePanel(){
-        // Calling login method
+        /* Calling login method */
         testLogin();
-        // clicking at MEEE
+        /* clicking at MEEE */
         nChrome.findElement(By.className("me")).click();
-        // Trying something that the teacher didnt teach yet, XPath!!
+        /* Trying something that the teacher didnt teach yet, XPath!! */
         WebElement textoNoH4 = nChrome.findElement(By.xpath("/html/body/div[1]/div/div/h4"));
-        // doing the assertion comparing the h4, bcuz it is only on this page, and it worked
+        /* doing the assertion comparing the h4, bcuz it is only on this page, and it worked */
         assertEquals("Hi, joaotn", textoNoH4.getText());
     }
 
     //ComboBoxes and Toast messages class
     @Test
     public void testAddMoreData(){
-        // Calling login method again
+        /* Calling login method again */
         testLogin();
-        // clicking at MEEE, I know, again, it is just to make it faster
+        /* clicking at MEEE, I know, again, it is just to make it faster */
         nChrome.findElement(By.className("me")).click();
-        // click at the link that contains the text MORE DATA ABOUT YOU
+        /* click at the link that contains the text MORE DATA ABOUT YOU */
         nChrome.findElement(By.linkText("MORE DATA ABOUT YOU")).click();
 
         nChrome.findElement(By.xpath("//*[@id=\"moredata\"]/div[2]/button")).click();
 
-        // now the "add more data about me" panel is open, lets select the combo and add the data
+        /* now the "add more data about me" panel is open, lets select the combo and add the data */
         WebElement popupAddMoreData = nChrome.findElement(By.id("addmoredata"));
 
         WebElement campoType = popupAddMoreData.findElement(By.name("type"));
         new Select(campoType).selectByVisibleText("Phone");
-        // clicked on phone now its just add the data
+        /* clicked on phone now its just add the data */
 
         popupAddMoreData.findElement(By.name("contact")).sendKeys("+5543991231234");
         popupAddMoreData.findElement(By.linkText("SAVE")).click();
 
-        // now the toast message
+        /* now the toast message */
 
         WebElement tMessage = nChrome.findElement(By.id("toast-container"));
         String message = tMessage.getText();
 
         assertEquals("Your contact has been added!", message);
     }
+    /* removing information test */
+    @Test
+    public void removeAddInfo(){
+        /* Im gonna call the login test */
+        testLogin();
+        nChrome.findElement(By.className("me")).click();
+        nChrome.findElement(By.linkText("MORE DATA ABOUT YOU")).click();
+        nChrome.findElement(By.xpath("//span[text()=\"+5543991231234\"]/following-sibling::a")).click();
+
+        /* Accepting the Json warning about the contact information removed */
+        nChrome.switchTo().alert().accept();
+
+        /* Now Im looking to the toast message warning me that the contact info was really removed */
+        WebElement tMessage = nChrome.findElement(By.id("toast-container"));
+        String message = tMessage.getText();
+
+        /* Classic assertion about the removing confirmation */
+        assertEquals("Rest in peace, dear phone!", message);
+
+        /* waiting 10 seconds be4 the toast message disappear  */
+        WebDriverWait cWait = new WebDriverWait(nChrome, 10);
+        cWait.until(ExpectedConditions.stalenessOf(tMessage));
+
+    }
 
     @After
     public void tearDown(){
-        //closing the browser
+        /* Classic logout added to finish every test case */
+        nChrome.findElement(By.linkText("Logout")).click();
+        /* closing the browser */
         nChrome.quit();
     }
 }
